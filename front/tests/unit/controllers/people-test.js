f0b24c58e7c8823cebadf3443c0dd72cb86c7934
Valeria <valeria.bugarin@haufe.com>
2019-02-06 16:45:31 +0100

    add unit test for controller

--- front/tests/unit/controllers/people-test.js
+++ front/tests/unit/controllers/people-test.js
@@ -6,7 +6,43 @@ module('Unit | Controller | people', function(hooks) {
 
   // Replace this with your real tests.
   test('it exists', function(assert) {
-    let controller = this.owner.lookup('controller:people');
+    const controller = this.owner.lookup('controller:people');
     assert.ok(controller);
+  });
+
+  test('set is call', function(assert) {
+    const controller = this.owner.lookup('controller:people');
+    const param = 'test';
+
+    controller.send('onFilter', param)
+
+    assert.equal(controller.get('param'), param);
+  });
+
+  test('computed property changes', function(assert) {
+    const controller = this.owner.lookup('controller:people');
+    const param = 'test';
+
+    const model = [{
+      company_name: '',
+      first_name: 'test',
+      gender: null,
+      id: '1068c3ed-1287-40ec-af6b-95a3c992abed',
+      last_name: 'test'
+    },
+    {
+      company_name: '',
+      first_name: 'alala',
+      gender: null,
+      id: '1068c3ed-1287-40ec-af6b-95a3c992abed',
+      last_name: 'Dylan'
+    }];
+
+    const result = [model[0]];
+
+    controller.set('model', model);
+    controller.send('onFilter', param)
+
+    assert.deepEqual(controller.get('peopleModel'), result);
   });
 });
